8f8094796aae1f61bb9e430f1d1cb8c21e831586
derover01 <85169813+derover01@users.noreply.github.com>
2026-08-30 19:01:18 +0200

    test(core): support mutable LuckPerms groups

--- plugins/SkyKings-Core/src/test/java/net/skykings/core/integration/luckperms/StubGroup.java
+++ plugins/SkyKings-Core/src/test/java/net/skykings/core/integration/luckperms/StubGroup.java
@@ -12,96 +12,31 @@ import java.util.Collection;
 import java.util.OptionalInt;
 import java.util.SortedSet;
 
-/**
- * Handgeschriebenes {@link Group}-Test-Double (siehe {@link StubLuckPerms} fuer den Grund).
- * Wird von {@link LuckPermsPermissionBridge} nur auf {@code null}/nicht-{@code null} geprueft
- * (existiert die Gruppe?) - keine Methode davon wird tatsaechlich aufgerufen.
- */
 final class StubGroup implements Group {
 
     private final String name;
+    private final StubNodeMap data = new StubNodeMap();
 
     StubGroup(String name) {
         this.name = name;
     }
 
-    @Override
-    public String getName() {
-        return name;
-    }
+    @Override public String getName() { return name; }
+    @Override public NodeMap data() { return data; }
+    StubNodeMap stubData() { return data; }
 
-    @Override
-    public String getDisplayName() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public String getDisplayName(QueryOptions queryOptions) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public OptionalInt getWeight() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public PermissionHolder.Identifier getIdentifier() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public String getFriendlyName() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public QueryOptions getQueryOptions() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public CachedDataManager getCachedData() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public NodeMap getData(DataType dataType) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public NodeMap data() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public NodeMap transientData() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public SortedSet<Node> getDistinctNodes() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public Collection<Node> resolveInheritedNodes(QueryOptions queryOptions) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public SortedSet<Node> resolveDistinctInheritedNodes(QueryOptions queryOptions) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public Collection<Group> getInheritedGroups(QueryOptions queryOptions) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public void auditTemporaryNodes() {
-        throw new UnsupportedOperationException();
-    }
+    @Override public String getDisplayName() { throw new UnsupportedOperationException(); }
+    @Override public String getDisplayName(QueryOptions queryOptions) { throw new UnsupportedOperationException(); }
+    @Override public OptionalInt getWeight() { throw new UnsupportedOperationException(); }
+    @Override public PermissionHolder.Identifier getIdentifier() { throw new UnsupportedOperationException(); }
+    @Override public String getFriendlyName() { throw new UnsupportedOperationException(); }
+    @Override public QueryOptions getQueryOptions() { throw new UnsupportedOperationException(); }
+    @Override public CachedDataManager getCachedData() { throw new UnsupportedOperationException(); }
+    @Override public NodeMap getData(DataType dataType) { throw new UnsupportedOperationException(); }
+    @Override public NodeMap transientData() { throw new UnsupportedOperationException(); }
+    @Override public SortedSet<Node> getDistinctNodes() { throw new UnsupportedOperationException(); }
+    @Override public Collection<Node> resolveInheritedNodes(QueryOptions queryOptions) { throw new UnsupportedOperationException(); }
+    @Override public SortedSet<Node> resolveDistinctInheritedNodes(QueryOptions queryOptions) { throw new UnsupportedOperationException(); }
+    @Override public Collection<Group> getInheritedGroups(QueryOptions queryOptions) { throw new UnsupportedOperationException(); }
+    @Override public void auditTemporaryNodes() { throw new UnsupportedOperationException(); }
 }
