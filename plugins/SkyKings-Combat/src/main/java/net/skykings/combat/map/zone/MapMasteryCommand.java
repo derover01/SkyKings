86faa2a471042a8b2c3f1b541f488bfb13a46c1d
derover01 <85169813+derover01@users.noreply.github.com>
2026-08-31 03:21:16 +0200

    Add map mastery command

---
+++ plugins/SkyKings-Combat/src/main/java/net/skykings/combat/map/zone/MapMasteryCommand.java
@@ -0,0 +1,41 @@
+package net.skykings.combat.map.zone;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+/** Zeigt Phase-6-Mastery fuer eigene oder fremde Online-Spieler. */
+public final class MapMasteryCommand implements CommandExecutor {
+    private final MapMasteryService mastery;
+
+    public MapMasteryCommand(MapMasteryService mastery) {
+        this.mastery = mastery;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        Player target;
+        if (args.length >= 1) {
+            target = Bukkit.getPlayer(args[0]);
+            if (target == null) {
+                sender.sendMessage(ChatColor.RED + "Spieler nicht online.");
+                return true;
+            }
+        } else {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage("Nutze /mapmastery <Spieler>.");
+                return true;
+            }
+            target = (Player) sender;
+        }
+
+        sender.sendMessage(ChatColor.GOLD.toString() + ChatColor.BOLD + "MAP MASTERY " + ChatColor.YELLOW + target.getName());
+        sender.sendMessage(ChatColor.GRAY + "Hot-Zone-Kills: " + ChatColor.WHITE + mastery.getHotZoneKills(target.getUniqueId()));
+        sender.sendMessage(ChatColor.GRAY + "King-Altar-Captures: " + ChatColor.WHITE + mastery.getKingCaptures(target.getUniqueId()));
+        sender.sendMessage(ChatColor.GRAY + "Titel: " + ChatColor.AQUA + mastery.getTitle(target.getUniqueId()));
+        return true;
+    }
+}
