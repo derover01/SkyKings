804e25932e06b9817510a9a2c9509c4f1ef66508
derover01 <85169813+derover01@users.noreply.github.com>
2026-09-01 18:10:47 +0200

    Extend player shop placement policy for rental sale checks

--- plugins/SkyKings-Core/src/main/java/net/skykings/core/shop/player/ShopPlacementPolicy.java
+++ plugins/SkyKings-Core/src/main/java/net/skykings/core/shop/player/ShopPlacementPolicy.java
@@ -3,11 +3,9 @@ package net.skykings.core.shop.player;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
-/**
- * Entkoppelt PlayerShops von konkreten Island-/Plot-Plugins.
- * Phase 7 kann diese Policy durch eine echte Owner/Member-Pruefung ersetzen.
- */
+/** Entkoppelt PlayerShops von konkreten Claim-/Rental-Systemen. */
 public interface ShopPlacementPolicy {
     boolean canCreateShop(Player player, Location location);
     boolean canManageShop(Player player, PlayerShop shop);
+    boolean canSellFromShop(PlayerShop shop);
 }
