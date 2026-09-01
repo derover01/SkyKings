e0b9dde25af8718357babd68ea3c6637416e0fd2
derover01 <85169813+derover01@users.noreply.github.com>
2026-09-02 01:30:32 +0200

    Add clear inventory command alias

--- plugins/SkyKings-Core/src/main/java/net/skykings/core/command/TrashCommand.java
+++ plugins/SkyKings-Core/src/main/java/net/skykings/core/command/TrashCommand.java
@@ -7,22 +7,33 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
-/** /trash bzw. /müll öffnet ein temporäres Inventar. Alles darin wird beim Schließen verworfen. */
+/** /trash oeffnet den Muell; /clearinv leert das komplette eigene Inventar. */
 public final class TrashCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!(sender instanceof Player)) {
-            sender.sendMessage("Dieser Befehl ist nur ingame verfügbar.");
+            sender.sendMessage("Dieser Befehl ist nur ingame verfuegbar.");
             return true;
         }
 
         Player player = (Player) sender;
-        Inventory trash = Bukkit.createInventory(null, 54, ChatColor.DARK_GRAY + "SkyKings | Müll");
+        if (label.equalsIgnoreCase("clearinv") || label.equalsIgnoreCase("clearinventory")) {
+            player.closeInventory();
+            player.getInventory().clear();
+            player.getInventory().setArmorContents(new ItemStack[4]);
+            player.setItemOnCursor(null);
+            player.updateInventory();
+            player.sendMessage(ChatColor.GREEN + "Dein Inventar wurde vollstaendig geleert.");
+            return true;
+        }
+
+        Inventory trash = Bukkit.createInventory(null, 54, ChatColor.DARK_GRAY + "SkyKings | Muell");
         player.openInventory(trash);
-        player.sendMessage(ChatColor.GRAY + "Alles, was du in dieses Inventar legst, wird beim Schließen "
-                + ChatColor.RED + "dauerhaft gelöscht" + ChatColor.GRAY + ".");
+        player.sendMessage(ChatColor.GRAY + "Alles, was du in dieses Inventar legst, wird beim Schliessen "
+                + ChatColor.RED + "dauerhaft geloescht" + ChatColor.GRAY + ".");
         return true;
     }
 }
