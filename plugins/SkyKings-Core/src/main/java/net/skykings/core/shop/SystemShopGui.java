3ecff6649f2791940bd7c660d499ffb204b72395
derover01 <85169813+derover01@users.noreply.github.com>
2026-09-02 01:40:10 +0200

    Rebalance OP PvP market and utility rights

--- plugins/SkyKings-Core/src/main/java/net/skykings/core/shop/SystemShopGui.java
+++ plugins/SkyKings-Core/src/main/java/net/skykings/core/shop/SystemShopGui.java
@@ -18,7 +18,7 @@ import org.bukkit.inventory.meta.ItemMeta;
 import java.util.ArrayList;
 import java.util.List;
 
-/** Premium System-Shop: Kategorien -> Progressionsseiten statt einer flachen Item-Wand. */
+/** Premium System-Shop fuer eine schnelle OP-SkyPvP-Economy mit echten High-End Coin-Sinks. */
 public final class SystemShopGui {
     private final GuiManager guiManager;
     private final ShopTransactionService transactions;
@@ -34,20 +34,20 @@ public final class SystemShopGui {
         GuiSession gui = GuiSession.create(player, UiTheme.title("Shop | Kategorien"), 54);
         gui.setItem(4, UiItems.item(Material.EMERALD,
                 UiTheme.PRIMARY + "SKYKINGS MARKET",
-                UiTheme.MUTED + "Erschaffe dir deine Ausruestung.",
-                UiTheme.WARNING + "Starke Items sind bewusst teuer.",
-                UiTheme.MUTED + "Erspielen soll wertvoll bleiben."));
+                UiTheme.MUTED + "Fight-Restocks bleiben bezahlbar.",
+                UiTheme.WARNING + "Prestige-Equipment verbrennt dagegen richtig Coins.",
+                UiTheme.MUTED + "Die Economy ist auf hohe Geldmengen ausgelegt."));
 
         category(gui, 10, Material.DIAMOND, ChatColor.GOLD + "Raenge", "Free-Rang-Progression", p -> p.performCommand("raenge"));
         category(gui, 12, Material.SKULL_ITEM, ChatColor.LIGHT_PURPLE + "Crates", "Crate & Reward Center", p -> p.performCommand("craterewards"));
-        category(gui, 14, Material.PAPER, ChatColor.YELLOW + "Rechte", "Permanente Komfort-Rechte", this::openRights);
+        category(gui, 14, Material.PAPER, ChatColor.YELLOW + "Rechte", "Permanente Commands & Komfort", this::openRights);
         category(gui, 16, Material.DIAMOND_SWORD, ChatColor.RED + "Schwerter", "Normal bis Sharpness XIII", this::openSwords);
         category(gui, 28, Material.BOW, ChatColor.GOLD + "Boegen", "Power Progression", this::openBows);
         category(gui, 30, Material.DIAMOND_CHESTPLATE, ChatColor.AQUA + "Ruestung", "PvP-Ruestung & OP-Tiers", this::openArmor);
-        category(gui, 32, Material.ENDER_PEARL, ChatColor.DARK_AQUA + "Enderperlen", "Kleine bis grosse Stacks", this::openPearls);
-        category(gui, 34, Material.COOKED_BEEF, ChatColor.GREEN + "Essen", "Food & Golden Apples", this::openFood);
-        category(gui, 38, Material.POTION, ChatColor.LIGHT_PURPLE + "Potions", "PvP-Verbrauchsgueter", this::openPotions);
-        category(gui, 40, Material.EXP_BOTTLE, ChatColor.WHITE + "Utility", "XP, Pfeile, Obsidian", this::openUtility);
+        category(gui, 32, Material.ENDER_PEARL, ChatColor.DARK_AQUA + "Enderperlen", "Fight-Restock zum fairen Preis", this::openPearls);
+        category(gui, 34, Material.GOLDEN_APPLE, ChatColor.GREEN + "Essen", "Food & OP-Golden-Apples", this::openFood);
+        category(gui, 38, Material.POTION, ChatColor.LIGHT_PURPLE + "Potions", "Speed II und Staerke II sind Premium-Restocks", this::openPotions);
+        category(gui, 40, Material.EXP_BOTTLE, ChatColor.WHITE + "Utility", "XP, Pfeile und Baumaterial", this::openUtility);
 
         gui.setItem(49, UiItems.item(Material.NETHER_STAR,
                 UiTheme.TEXT + "Dein Guthaben",
@@ -58,19 +58,19 @@ public final class SystemShopGui {
     }
 
     private void openSwords(Player p) {
-        GuiSession gui = page(p, "Schwerter", Material.DIAMOND_SWORD, "Von Einstieg bis Prestige-Waffe");
+        GuiSession gui = page(p, "Schwerter", Material.DIAMOND_SWORD, "Fight-Gear bezahlbar, Prestige brutal teuer");
         add(gui,p,10,offer("sword-iron", enchanted(Material.IRON_SWORD,"&fStarter Sword",1,1,0),150000L),"SYSTEM_SWORDS");
         add(gui,p,11,offer("sword-d1", enchanted(Material.DIAMOND_SWORD,"&aPvP Sword",1,2,0),450000L),"SYSTEM_SWORDS");
-        add(gui,p,12,offer("sword-d3", enchanted(Material.DIAMOND_SWORD,"&bElite Sword",3,3,1),1750000L),"SYSTEM_SWORDS");
-        add(gui,p,13,offer("sword-d5", enchanted(Material.DIAMOND_SWORD,"&dTitan Sword",5,3,2),5500000L),"SYSTEM_SWORDS");
-        add(gui,p,14,offer("sword-d7", enchanted(Material.DIAMOND_SWORD,"&6Legend Blade",7,5,2),15000000L),"SYSTEM_SWORDS");
-        add(gui,p,15,offer("sword-d10", enchanted(Material.DIAMOND_SWORD,"&cOverlord Blade",10,7,2),45000000L),"SYSTEM_SWORDS");
-        add(gui,p,16,offer("sword-d13", enchanted(Material.DIAMOND_SWORD,"&5&lMYTHIC SHARPNESS XIII",13,10,3),125000000L),"SYSTEM_SWORDS");
+        add(gui,p,12,offer("sword-d3", enchanted(Material.DIAMOND_SWORD,"&bElite Sword",3,3,1),2000000L),"SYSTEM_SWORDS");
+        add(gui,p,13,offer("sword-d5", enchanted(Material.DIAMOND_SWORD,"&dTitan Sword",5,3,2),10000000L),"SYSTEM_SWORDS");
+        add(gui,p,14,offer("sword-d7", enchanted(Material.DIAMOND_SWORD,"&6Legend Blade",7,5,2),40000000L),"SYSTEM_SWORDS");
+        add(gui,p,15,offer("sword-d10", enchanted(Material.DIAMOND_SWORD,"&cOverlord Blade",10,7,2),150000000L),"SYSTEM_SWORDS");
+        add(gui,p,16,offer("sword-d13", enchanted(Material.DIAMOND_SWORD,"&5&lMYTHIC SHARPNESS XIII",13,10,3),1000000000L),"SYSTEM_SWORDS");
         nav(gui); guiManager.open(gui); SoundFeedback.menuOpen(p);
     }
 
     private void openBows(Player p) {
-        GuiSession gui = page(p,"Boegen",Material.BOW,"Power steigt - Preis explodiert");
+        GuiSession gui = page(p,"Boegen",Material.BOW,"Power steigt - High-End bleibt ein Coin-Sink");
         add(gui,p,10,offer("bow-p1", bow("&fStarter Bow",1,0,0),200000L),"SYSTEM_BOWS");
         add(gui,p,11,offer("bow-p3", bow("&aHunter Bow",3,1,0),900000L),"SYSTEM_BOWS");
         add(gui,p,12,offer("bow-p5", bow("&bElite Bow",5,1,1),3500000L),"SYSTEM_BOWS");
@@ -80,7 +80,7 @@ public final class SystemShopGui {
     }
 
     private void openArmor(Player p) {
-        GuiSession gui = page(p,"Ruestung",Material.DIAMOND_CHESTPLATE,"Einzelteile - keine billigen Komplettsets");
+        GuiSession gui = page(p,"Ruestung",Material.DIAMOND_CHESTPLATE,"Einzelteile - OP-Tiers bleiben wertvoll");
         add(gui,p,10,offer("armor-helm-p2", armor(Material.DIAMOND_HELMET,"&aGuardian Helm",2,2),650000L),"SYSTEM_ARMOR");
         add(gui,p,11,offer("armor-chest-p2", armor(Material.DIAMOND_CHESTPLATE,"&aGuardian Chest",2,2),950000L),"SYSTEM_ARMOR");
         add(gui,p,12,offer("armor-legs-p2", armor(Material.DIAMOND_LEGGINGS,"&aGuardian Legs",2,2),850000L),"SYSTEM_ARMOR");
@@ -94,62 +94,66 @@ public final class SystemShopGui {
     }
 
     private void openPearls(Player p) {
-        GuiSession gui = page(p,"Enderperlen",Material.ENDER_PEARL,"Mobilitaet bleibt ein echter Coin-Sink");
-        add(gui,p,11,new ShopOffer("pearls-8",new ItemStack(Material.ENDER_PEARL,8),ShopCurrency.COINS,140000L),"SYSTEM_PEARLS");
-        add(gui,p,13,new ShopOffer("pearls-16",new ItemStack(Material.ENDER_PEARL,16),ShopCurrency.COINS,275000L),"SYSTEM_PEARLS");
-        add(gui,p,15,new ShopOffer("pearls-32",new ItemStack(Material.ENDER_PEARL,32),ShopCurrency.COINS,600000L),"SYSTEM_PEARLS");
-        add(gui,p,31,new ShopOffer("pearls-64",new ItemStack(Material.ENDER_PEARL,64),ShopCurrency.COINS,1350000L),"SYSTEM_PEARLS");
+        GuiSession gui = page(p,"Enderperlen",Material.ENDER_PEARL,"Mobilitaet soll jederzeit nachkaufbar sein");
+        add(gui,p,11,new ShopOffer("pearls-8",new ItemStack(Material.ENDER_PEARL,8),ShopCurrency.COINS,65000L),"SYSTEM_PEARLS");
+        add(gui,p,13,new ShopOffer("pearls-16",new ItemStack(Material.ENDER_PEARL,16),ShopCurrency.COINS,125000L),"SYSTEM_PEARLS");
+        add(gui,p,15,new ShopOffer("pearls-32",new ItemStack(Material.ENDER_PEARL,32),ShopCurrency.COINS,250000L),"SYSTEM_PEARLS");
+        add(gui,p,31,new ShopOffer("pearls-64",new ItemStack(Material.ENDER_PEARL,64),ShopCurrency.COINS,500000L),"SYSTEM_PEARLS");
         nav(gui); guiManager.open(gui); SoundFeedback.menuOpen(p);
     }
 
     private void openFood(Player p) {
-        GuiSession gui = page(p,"Essen",Material.COOKED_BEEF,"Grundversorgung bis PvP-Luxus");
-        add(gui,p,11,new ShopOffer("food-steak-16",new ItemStack(Material.COOKED_BEEF,16),ShopCurrency.COINS,35000L),"SYSTEM_FOOD");
-        add(gui,p,12,new ShopOffer("food-steak-64",new ItemStack(Material.COOKED_BEEF,64),ShopCurrency.COINS,120000L),"SYSTEM_FOOD");
-        add(gui,p,14,new ShopOffer("food-gapples-8",new ItemStack(Material.GOLDEN_APPLE,8),ShopCurrency.COINS,350000L),"SYSTEM_FOOD");
-        add(gui,p,15,new ShopOffer("food-opgaps-2",new ItemStack(Material.GOLDEN_APPLE,2,(short)1),ShopCurrency.COINS,900000L),"SYSTEM_FOOD");
-        add(gui,p,31,new ShopOffer("food-opgaps-8",new ItemStack(Material.GOLDEN_APPLE,8,(short)1),ShopCurrency.COINS,4200000L),"SYSTEM_FOOD");
+        GuiSession gui = page(p,"Essen",Material.GOLDEN_APPLE,"Fight-Food ist Restock, kein Luxusgut");
+        add(gui,p,10,new ShopOffer("food-steak-16",new ItemStack(Material.COOKED_BEEF,16),ShopCurrency.COINS,25000L),"SYSTEM_FOOD");
+        add(gui,p,11,new ShopOffer("food-steak-64",new ItemStack(Material.COOKED_BEEF,64),ShopCurrency.COINS,75000L),"SYSTEM_FOOD");
+        add(gui,p,13,new ShopOffer("food-gapples-16",new ItemStack(Material.GOLDEN_APPLE,16),ShopCurrency.COINS,30000L),"SYSTEM_FOOD");
+        add(gui,p,14,new ShopOffer("food-gapples-64",new ItemStack(Material.GOLDEN_APPLE,64),ShopCurrency.COINS,100000L),"SYSTEM_FOOD");
+        add(gui,p,16,new ShopOffer("food-opgaps-16",new ItemStack(Material.GOLDEN_APPLE,16,(short)1),ShopCurrency.COINS,70000L),"SYSTEM_FOOD");
+        add(gui,p,31,new ShopOffer("food-opgaps-64",new ItemStack(Material.GOLDEN_APPLE,64,(short)1),ShopCurrency.COINS,250000L),"SYSTEM_FOOD");
         nav(gui); guiManager.open(gui); SoundFeedback.menuOpen(p);
     }
 
     private void openPotions(Player p) {
-        GuiSession gui = page(p,"Potions",Material.POTION,"Verbrauchsgueter sind absichtlich nicht billig");
-        add(gui,p,11,new ShopOffer("pot-heal2-4",new ItemStack(Material.POTION,4,(short)16421),ShopCurrency.COINS,400000L),"SYSTEM_POTIONS");
-        add(gui,p,13,new ShopOffer("pot-speed2-4",new ItemStack(Material.POTION,4,(short)8226),ShopCurrency.COINS,450000L),"SYSTEM_POTIONS");
-        add(gui,p,15,new ShopOffer("pot-strength2-4",new ItemStack(Material.POTION,4,(short)8233),ShopCurrency.COINS,650000L),"SYSTEM_POTIONS");
-        add(gui,p,29,new ShopOffer("pot-heal2-12",new ItemStack(Material.POTION,12,(short)16421),ShopCurrency.COINS,1150000L),"SYSTEM_POTIONS");
-        add(gui,p,31,new ShopOffer("pot-speed2-12",new ItemStack(Material.POTION,12,(short)8226),ShopCurrency.COINS,1300000L),"SYSTEM_POTIONS");
-        add(gui,p,33,new ShopOffer("pot-strength2-12",new ItemStack(Material.POTION,12,(short)8233),ShopCurrency.COINS,1850000L),"SYSTEM_POTIONS");
+        GuiSession gui = page(p,"Potions",Material.POTION,"Speed II und Staerke II sind wichtige Premium-Restocks");
+        add(gui,p,10,new ShopOffer("pot-heal2-4",new ItemStack(Material.POTION,4,(short)16421),ShopCurrency.COINS,500000L),"SYSTEM_POTIONS");
+        add(gui,p,12,new ShopOffer("pot-speed2-1",new ItemStack(Material.POTION,1,(short)8226),ShopCurrency.COINS,2500000L),"SYSTEM_POTIONS");
+        add(gui,p,14,new ShopOffer("pot-strength2-1",new ItemStack(Material.POTION,1,(short)8233),ShopCurrency.COINS,2500000L),"SYSTEM_POTIONS");
+        add(gui,p,30,new ShopOffer("pot-heal2-12",new ItemStack(Material.POTION,12,(short)16421),ShopCurrency.COINS,1400000L),"SYSTEM_POTIONS");
+        add(gui,p,32,new ShopOffer("pot-speed2-4",new ItemStack(Material.POTION,4,(short)8226),ShopCurrency.COINS,9000000L),"SYSTEM_POTIONS");
+        add(gui,p,34,new ShopOffer("pot-strength2-4",new ItemStack(Material.POTION,4,(short)8233),ShopCurrency.COINS,9000000L),"SYSTEM_POTIONS");
         nav(gui); guiManager.open(gui); SoundFeedback.menuOpen(p);
     }
 
     private void openUtility(Player p) {
-        GuiSession gui = page(p,"Utility",Material.EXP_BOTTLE,"Restock ohne Progression zu entwerten");
-        add(gui,p,10,new ShopOffer("util-arrows",new ItemStack(Material.ARROW,64),ShopCurrency.COINS,85000L),"SYSTEM_UTILITY");
-        add(gui,p,12,new ShopOffer("util-xp",new ItemStack(Material.EXP_BOTTLE,32),ShopCurrency.COINS,250000L),"SYSTEM_UTILITY");
-        add(gui,p,14,new ShopOffer("util-obsidian",new ItemStack(Material.OBSIDIAN,32),ShopCurrency.COINS,300000L),"SYSTEM_UTILITY");
-        add(gui,p,16,new ShopOffer("util-anvil",new ItemStack(Material.ANVIL,1),ShopCurrency.COINS,750000L),"SYSTEM_UTILITY");
+        GuiSession gui = page(p,"Utility",Material.EXP_BOTTLE,"Alltagsmaterial bleibt bewusst guenstig");
+        add(gui,p,10,new ShopOffer("util-arrows",new ItemStack(Material.ARROW,64),ShopCurrency.COINS,50000L),"SYSTEM_UTILITY");
+        add(gui,p,12,new ShopOffer("util-xp",new ItemStack(Material.EXP_BOTTLE,32),ShopCurrency.COINS,150000L),"SYSTEM_UTILITY");
+        add(gui,p,14,new ShopOffer("util-obsidian",new ItemStack(Material.OBSIDIAN,64),ShopCurrency.COINS,100000L),"SYSTEM_UTILITY");
+        add(gui,p,16,new ShopOffer("util-anvil",new ItemStack(Material.ANVIL,1),ShopCurrency.COINS,100000L),"SYSTEM_UTILITY");
         nav(gui); guiManager.open(gui); SoundFeedback.menuOpen(p);
     }
 
     private void openRights(Player p) {
-        GuiSession gui = page(p,"Rechte",Material.PAPER,"Permanente Rechte - einmal kaufen");
+        GuiSession gui = page(p,"Rechte",Material.PAPER,"Permanente Commands statt teurer Block-Gimmicks");
+        String[] ids = {"enderchest","workbench","anvil","fly","repair","stack","enchantmenttable","statsreset"};
         int[] slots = {10,12,14,16,28,30,32,34};
-        int index = 0;
-        for (VoucherPermission right : rights.getAll()) {
-            if (index >= slots.length) break;
-            final VoucherPermission selected = right;
-            final long price = rightPrice(right.getId());
-            boolean owned = p.hasPermission(right.getNode());
-            List<String> lore = new ArrayList<String>();
-            lore.add(UiTheme.MUTED + "Permanent fuer diesen Account");
-            lore.add(UiTheme.MUTED + "Preis " + UiTheme.TEXT + UiFormat.number(price) + " Coins");
-            lore.add(owned ? UiTheme.SUCCESS + "BEREITS FREIGESCHALTET" : UiItems.action("Klicken zum Kaufen"));
-            gui.setItem(slots[index++], UiItems.item(Material.PAPER,
-                    (owned ? UiTheme.SUCCESS : UiTheme.TEXT) + right.getDisplayName(), lore.toArray(new String[lore.size()])),
-                    owned ? null : (player,event,slot) -> confirmRight(player, selected, price));
+        for (int i = 0; i < ids.length; i++) {
+            VoucherPermission right = rights.find(ids[i]);
+            if (right != null) addRight(gui, p, slots[i], right);
         }
         nav(gui); guiManager.open(gui); SoundFeedback.menuOpen(p);
+    }
+
+    private void addRight(GuiSession gui, Player p, int slot, final VoucherPermission right) {
+        final long price = rightPrice(right.getId());
+        boolean owned = p.hasPermission(right.getNode());
+        List<String> lore = new ArrayList<String>();
+        lore.add(UiTheme.MUTED + "Permanent fuer diesen Account");
+        lore.add(UiTheme.MUTED + "Preis " + UiTheme.TEXT + UiFormat.number(price) + " Coins");
+        lore.add(owned ? UiTheme.SUCCESS + "BEREITS FREIGESCHALTET" : UiItems.action("Klicken zum Kaufen"));
+        gui.setItem(slot, UiItems.item(Material.PAPER,
+                (owned ? UiTheme.SUCCESS : UiTheme.TEXT) + right.getDisplayName(), lore.toArray(new String[lore.size()])),
+                owned ? null : (player,event,clicked) -> confirmRight(player, right, price));
     }
 
     private void confirmRight(final Player p, final VoucherPermission right, final long price) {
@@ -177,10 +181,14 @@ public final class SystemShopGui {
     private long rightPrice(String id) {
         if (id == null) return 10000000L;
         String key = id.toLowerCase();
-        if (key.contains("fly")) return 25000000L;
-        if (key.contains("repair")) return 18000000L;
-        if (key.contains("stack")) return 12000000L;
-        if (key.contains("bloecke")) return 15000000L;
+        if (key.contains("fly")) return 35000000L;
+        if (key.contains("repair")) return 20000000L;
+        if (key.contains("stack")) return 15000000L;
+        if (key.contains("enderchest")) return 15000000L;
+        if (key.contains("workbench")) return 5000000L;
+        if (key.contains("anvil")) return 7500000L;
+        if (key.contains("enchantment")) return 7500000L;
+        if (key.contains("statsreset")) return 50000000L;
         return 10000000L;
     }
 
